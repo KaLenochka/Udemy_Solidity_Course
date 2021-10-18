399248847528a1a785d0fdb8eeccd8e22d2b75f1
Elena Dolinchuk <84373882+KaLenochka@users.noreply.github.com>
2021-10-18 13:06:49 +0300

    Create variables-and-types.sol

---
+++ variables-and-types.sol
@@ -0,0 +1,16 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+//Three main types of variables: Boolings(true/false), Integers(numbers uint/int) and Strings(sequences of characters)
+
+contract learnVariables {
+    // all our solidity code
+    
+    uint chocolateBar = 10;
+    uint storeOwner = 300;
+    bool lieDetector = true;
+    string errorMessageText = 'Error! There has been a mistake!';
+    
+    uint wallet = 500;
+    bool spend = false;
+    string notifySpend = 'You have spent money!';
+}
