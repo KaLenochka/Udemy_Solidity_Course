f1ff028c6d471cbd442aa9bc5f3f78a78bb49668
Elena Dolinchuk <84373882+KaLenochka@users.noreply.github.com>
2021-10-25 21:31:02 +0300

    Added the exercise and nested mapping

--- mapping.sol
+++ mapping.sol
@@ -17,3 +17,35 @@ contract LearnMapping {
         delete myMap[_addr];
     }
 }
+
+
+// Mapping Assignment:
+    // 0. Create a unique data type as a struct called Movie and give it the string properties: title and diretor.
+    // 1. Create a map called movie which takes a uint as a key and Movie as a value 
+    // 2. create a function called addMovie which takes three inputs, movie id, title and director which 
+    // assigns a value of an integer to a movie added back to the movie map. It should include a title and director name.
+    // 3. Deploy the contract and update the movie information to the movie map with our favorite movies! 
+    
+contract MappingAssignment {
+    
+    struct Movie {
+        string title;
+        string director;
+    }
+    
+    mapping(uint => Movie) movie;
+    mapping(address => mapping(uint => Movie)) public myMovie;
+    
+    function addMovie(uint movie_id, string memory title, string memory director) public {
+        movie[movie_id] = Movie(title, director);
+    }
+    
+    function addMyMovie(uint movie_id, string memory title, string memory director) public {
+        myMovie[msg.sender][movie_id] = Movie(title, director);
+        // msg.sender is a global variable accessable throughout solidity with the captures the address 
+        //that is calling the contract
+    }
+}
+
+// NESTED MAPPING (maps with maps)
+// ex. mapping(key1 => mapping(key2 => value2))
