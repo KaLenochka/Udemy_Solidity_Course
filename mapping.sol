e15e8d9e22de36e430c1cefed000105a30d00078
Elena Dolinchuk <84373882+KaLenochka@users.noreply.github.com>
2021-10-25 20:48:23 +0300

    Create mapping.sol

---
+++ mapping.sol
@@ -0,0 +1,19 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract LearnMapping {
+    
+    mapping(address => uint) public myMap;
+    
+    function getAddress(address _addr) public view returns(uint) {
+        return myMap[_addr];
+    }
+    
+    function setAddress(address _addr, uint _i) public {
+        myMap[_addr] = _i;
+        
+    }
+    
+    function removeAddress(address _addr) public {
+        delete myMap[_addr];
+    }
+}
